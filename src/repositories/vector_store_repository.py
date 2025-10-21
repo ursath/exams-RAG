7577552874095084b2fe71f94ff0081f6cfbdd23
UrielArias <urielarias.a12@gmail.com>
2025-10-21 00:45:18 -0300

    feat: adding vector store service and repository

---
+++ src/repositories/vector_store_repository.py
@@ -0,0 +1,56 @@
+from pinecone.grpc import PineconeGRPC as Pinecone
+from pinecone import ServerlessSpec
+from pinecone import Index # Assuming pc is already imported and initialized
+from services.environment_service import environment_service
+from typing import List, Dict, Any
+
+
+class VectorStoreRepository:
+    def __init__(self, index_name:str, vector_dim:int, sim_method:str="cosine"):
+        self.sim_method = sim_method
+        self.index_name = index_name
+        self.pc = Pinecone(environment_service.get_pinecone_api_key())
+        self.init_repository(index_name, vector_dim, sim_method)
+
+    def init_repository(self, index_name:str = "notes", vector_dimension:int = 1536, similarity_method = "cosine"):
+
+        if index_name not in self.pc.list_indexes().names:
+            self.pc.create_index(
+                name=index_name,
+                dimension=vector_dimension,
+                metric=similarity_method,
+                spec=ServerlessSpec(
+                    cloud="aws",       
+                    region="sa-east-1" 
+                )
+            )
+        self.index = self.pc.Index(index_name)
+
+    #metadata should be a list where each element is a dict like this (for each embedding): {"subject": "SO", "topics": ["pipes", "shared memory", "sockets"], "main topic": "IPC"}
+    def store(self, docs, batch_size:int=256):
+        total_vectors = len(docs)
+        
+        for i in range(0, total_vectors, batch_size):
+            batch = docs[i : i + batch_size]
+            self.index.upsert(vectors=batch)
+       
+    def retrieve(self, query_embedding: List[int], metadata: dict, top_k:int = 5): 
+        
+        metadata_filter = {
+            "$and": [
+                {"subject": {"$eq": metadata['subject']}},
+                {"$or": [
+                    {"main topic": {"$eq": metadata['main topic']}},
+                    {"topics": {"$eq": metadata['main topic']}}
+                ]}
+            ]
+        }
+
+        results = self.index.query(
+            vector = query_embedding,
+            top_k = top_k, 
+            filter= metadata_filter,
+            include_metadata=True
+        )
+
+        return results
---
+++ src/services/vector_store_service.py
@@ -0,0 +1,53 @@
+from repositories.vector_store_repository import VectorStoreRepository
+from embedding_service import EmbeddingService
+from typing import List
+from langchain_core.documents import Document
+
+class VectorStoreService:
+    def __init__(self, index_name:str, vector_dim:int, sim_method:str="cosine"):
+        self.repository = VectorStoreRepository(index_name, vector_dim, sim_method)
+        self.embedder = EmbeddingService()
+
+    def store(self, docs: List[Document], embedding_model: str):
+        embeddings, metadata = self.process_docs(docs, embedding_model)
+        self.repository.store(self, embeddings, metadata)
+
+    def prepare_doc_format(id: str, values: List[int], metadata: dict)-> dict:
+        doc = {}
+        doc['id'] = id
+        doc['values'] = values 
+        doc['metadata'] = metadata
+        return doc
+
+    def process_docs(self, docs: List[Document], embedding_model: str):
+        raw_chunks= []
+        metadata = []
+        for doc in docs:
+            raw_chunks.append(doc.page_content)
+            metadata.append(doc.metadata)
+        
+        embeddings = self.embedder.embed_chunks(raw_chunks, embedding_model)
+
+        processed_docs = []
+
+        for embedding, meta in zip(embeddings, metadata):
+            id = f"{self.repository.index_name}-{meta['subject']}-{meta['main topic']}-{"-".join(meta['topics'])}" 
+            doc = self.prepare_doc_format(id, embedding, meta)
+            processed_docs.append(doc)
+
+        return processed_docs 
+    
+
+    def filter_out_by_threshold(self, results: object, threshold:float):
+        filtered_results = []
+        for match in results.matches:
+            if (match.score >= threshold):
+                filtered_results.append(match)
+            else:
+                continue
+        return filtered_results
+
+    def retrieve(self, query_embedding: List[int], query_metadata: dict, threshold:float = 0.25, top_k:int = 5): 
+        response = self.repository.retrieve(query_embedding, query_metadata, top_k)
+        response_filtered = self.filter_out_by_threshold(response, threshold)
+        return response_filtered
