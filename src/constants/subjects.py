6a7b6fea05fe99d66efa5854abd75e1e7ded967b
mquesada02 <mquesada@itba.edu.ar>
2025-10-23 22:57:03 -0300

    chore: added subjects map

---
+++ src/constants/subjects.py
@@ -0,0 +1,23 @@
+subjects = [
+  {
+    "subject": "operating_systems",
+    "i18n": {
+      "es": "Sistemas Operativos",
+      "en": "Operating Systems"
+    }
+  },
+  {
+    "subject": "software_project_management",
+    "i18n": {
+      "es": "Gestión de Proyectos Informáticos",
+      "en": "Software Project Management"
+    }
+  },
+  {
+    "subject": "occupational_and_environmental_safety",
+    "i18n": {
+      "es": "Seguridad Ocupacional y Ambiental",
+      "en": "Occupational and Environmental Safety"
+    }
+  }
+]
