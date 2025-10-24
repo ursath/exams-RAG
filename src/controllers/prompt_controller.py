114f53ab5019e215505215bb924ff97896db207d
mquesada02 <mquesada@itba.edu.ar>
2025-10-24 04:38:29 -0300

    chore: fixed json formatting on SSE

--- src/controllers/prompt_controller.py
+++ src/controllers/prompt_controller.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, request, Response, stream_with_context
 from src.services.prompt_service import PromptService, prompt_service
+import json
 
 prompt_router = Blueprint("prompt", __name__, url_prefix="/prompt")
 
@@ -12,6 +13,7 @@ def create_prompt():
   @stream_with_context
   def sse_wrapper():
     for chunk in generator:
-      yield f"event: message_delta\ndata: {{\"data\": \"{chunk}\"}}\n\n"
+      data = json.dumps({"data": chunk})
+      yield f"event: message_delta\ndata: {data}\n\n"
     yield "event: done\ndata: [DONE]\n\n"
   return Response(sse_wrapper(), mimetype="text/event-stream")
